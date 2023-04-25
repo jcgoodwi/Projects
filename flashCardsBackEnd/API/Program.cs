706bbedcc117b8298ac53b3ec2ad61dbefbf0c95
jcgoodwi <124630882+jcgoodwi@users.noreply.github.com>
2023-04-25 14:04:46 -0700

    Update Program.cs

--- flashCardsBackEnd/API/Program.cs
+++ flashCardsBackEnd/API/Program.cs
@@ -2,11 +2,32 @@ using DataAccess;
 using Microsoft.EntityFrameworkCore;
 using DataAccess.Entities;
 
+var  MyAllowSpecificOrigins = "*";
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddCors(options =>  
+{  
+    options.AddPolicy(name: MyAllowSpecificOrigins,  
+        policy  =>  
+        {  
+            policy.WithOrigins(MyAllowSpecificOrigins)
+            .AllowAnyHeader()
+            .AllowAnyMethod(); // add the allowed origins  
+        });  
+});  
 
 builder.Services.AddScoped<Repository>();
 builder.Services.AddDbContext<FlashcardDbContext>((options) => options.UseSqlServer(builder.Configuration.GetConnectionString("FlashCardDB")));
-
+builder.Services.AddCors(opt =>
+        {
+            opt.AddPolicy(name: "OpenAccess", builder =>
+            {
+                builder.AllowAnyOrigin()
+                    .AllowAnyHeader()
+                    .AllowAnyMethod();
+            });
+        });
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -24,5 +45,7 @@ app.UseHttpsRedirection();
 app.UseAuthorization();
 
 app.MapControllers();
+
+app.UseCors(MyAllowSpecificOrigins);
 
 app.Run();
